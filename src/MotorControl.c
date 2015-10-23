ec2d396771ee1ff880555c40482790d0f9dda433
markz114 <mzhao114@gmail.com>
2015-10-23 15:12:03 -0400

    Re Formatted Files

--- src/MotorControl.c
+++ src/MotorControl.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <mraa.h>
 
 int main() {
   /* Setup your example here, code that should run once
    */
 
-	print('a');
+
   /* Code in this loop will run repeatedly
    */
   for (;;) {

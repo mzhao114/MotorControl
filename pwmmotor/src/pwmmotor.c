353acd69b90a5a9b842cf4d8ebc2ed6f14a2f86f
markz114 <mzhao114@gmail.com>
2015-10-23 15:06:20 -0400

    Created test wpm

---
+++ pwmmotor/src/pwmmotor.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mraa.h>
+
+int main() {
+  /* Setup your example here, code that should run once
+   */
+
+	print('a');
+  /* Code in this loop will run repeatedly
+   */
+  for (;;) {
+
+  }
+
+  return 0;
+}
